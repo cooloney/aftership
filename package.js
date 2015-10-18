d5a5d278236a734d89c5471fe0f9a8b00a10b466
Bryan Wu <cooloney@gmail.com>
2015-10-18 01:21:30 -0700

    Init commit
    Signed-off-by: Bryan Wu <cooloney@gmail.com>

---
+++ package.js
@@ -0,0 +1,28 @@
+Package.describe({
+  name: 'maodou:aftership',
+  version: '0.0.1',
+  // Brief, one-line summary of the package.
+  summary: 'A Meteor wrap for npm aftership package',
+  // URL to the Git repository containing the source code for this package.
+  git: '',
+  // By default, Meteor will default to using README.md for documentation.
+  // To avoid submitting documentation, set this field to null.
+  documentation: 'README.md'
+});
+
+Npm.depends({
+  "aftership": "4.1.4"
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.2.0.2');
+  api.use('ecmascript');
+  api.addFiles('aftership.js');
+});
+
+Package.onTest(function(api) {
+  api.use('ecmascript');
+  api.use('tinytest');
+  api.use('maodou:aftership');
+  api.addFiles('aftership-tests.js');
+});
