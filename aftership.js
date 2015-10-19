e0af2dd82f090fff7d361116216c9fe7d23d0eac
Bryan Wu <cooloney@gmail.com>
2015-10-19 12:10:42 -0700

    Add all sync version of Aftership API

--- aftership.js
+++ aftership.js
@@ -1,1 +1,44 @@
-// Write your package code here!
+aftership = Npm.require('aftership');
+
+Aftership = function (key) {
+  var _aftership = new aftership(key);
+
+  return {
+    'createTracking': function (trackingNumber, params) {
+        return Meteor.wrapAsync(_aftership.createTracking)(trackingNumber, params);
+    },
+
+    'getTracking': function (slug, trackingNumber, options) {
+      return Meteor.wrapAsync(_aftership.getTracking)(slug, trackingNumber, options);
+    },
+
+    'getTrackings': function (options) {
+      return Meteor.wrapAsync(_aftership.getTracking)(options);
+    },
+
+    'updateTracking': function(slug, trackingNumber, options) {
+        return Meteor.wrapAsync(_aftership.updateTracking)(slug, trackingNumber, options);
+    },
+
+    'retrackTracking': function(slug, trackingNumber, options) {
+        return Meteor.wrapAsync(_aftership.retrackTracking)(slug, trackingNumber, options);
+    },
+
+    'deleteTracking': function(slug, trackingNumber, requiredFields) {
+        return Meteor.wrapAsync(_aftership.deleteTracking)(slug, trackingNumber, requiredFields);
+    },
+
+    'getLastCheckpoint': function (slug, trackingNumber, options) {
+        return Meteor.wrapAsync(_aftership.getLastCheckpoint)(slug, trackingNumber, options);
+    },
+
+    'getCouriers': function () {
+        return Meteor.wrapAsync(_aftership.getCouriers)();
+    },
+
+    'detectCouriers': function(trackingNumber, requiredFields, detectMode) {
+        return Meteor.wrapAsync(_aftership.detectCouriers)(trackingNumber, requiredFields, detectMode);
+    }
+  };
+};
+
--- package.js
+++ package.js
@@ -1,10 +1,10 @@
 Package.describe({
-  name: 'maodou:aftership',
+  name: 'cooloney:aftership',
   version: '0.0.1',
   // Brief, one-line summary of the package.
   summary: 'A Meteor wrap for npm aftership package',
   // URL to the Git repository containing the source code for this package.
-  git: '',
+  git: 'https://github.com/cooloney/aftership',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
@@ -17,12 +17,13 @@ Npm.depends({
 Package.onUse(function(api) {
   api.versionsFrom('1.2.0.2');
   api.use('ecmascript');
-  api.addFiles('aftership.js');
+  api.addFiles('aftership.js', 'server');
+  api.export('Aftership', 'server');
 });
 
 Package.onTest(function(api) {
   api.use('ecmascript');
   api.use('tinytest');
-  api.use('maodou:aftership');
+  api.use('cooloney:aftership');
   api.addFiles('aftership-tests.js');
 });
