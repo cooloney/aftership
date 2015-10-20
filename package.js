fee5ecaa9180a2b189bb9687da9836a0a76fc8de
Bryan Wu <cooloney@gmail.com>
2015-10-20 15:31:07 -0700

    Bump version to 0.0.3

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'cooloney:aftership',
-  version: '0.0.2',
+  version: '0.0.3',
   // Brief, one-line summary of the package.
   summary: 'A Meteor wrap for npm aftership package',
   // URL to the Git repository containing the source code for this package.
